0e55ab69858ac7d0e0379a3a1c80cf906f729e1a
Otavio <otavio.zerotwo@gmail.com>
2024-10-13 22:28:57 -0300

    feat: add content HTML + CSS + JS

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburgerButton = document.querySelector('#hamburgerButton');
+const closeButton = document.querySelector('#closeButton');
+
+const mobileMenu = document.querySelector('#mobileMenu');
+
+hamburgerButton.addEventListener('click', function() {
+  // mobileMenu.classList.add('flex');
+  alert('OK');
+});
+
+closeButton.addEventListener('click', function() {
+  mobileMenu.classList.remove('flex');
+});
